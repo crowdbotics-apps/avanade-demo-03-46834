001e6016824c5907f52841c4edad8421830cba09
Crowdbotics <team@crowdbotics.com>
2024-03-06 10:28:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "avanade-demo-03-46834",
+  url: "https://avanade-demo-03-46834.botics.co",
+  api: "https://avanade-demo-03-46834.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
